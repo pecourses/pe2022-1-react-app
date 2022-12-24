d950df88a00e8f6ab1f09408a9f6c0480e2fc07a
Viktoriia <pecourses>
2022-12-24 19:27:19 +0200

    Add isVisible

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { Component } from 'react';
 import StopWatch from './StopWatch';
 
-function App() {
-  return <StopWatch />;
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isVisible: false,
+    };
+  }
+
+  handleClick = () => {
+    const { isVisible } = this.state;
+    this.setState({ isVisible: !isVisible });
+  };
+
+  render() {
+    const { isVisible } = this.state;
+    return (
+      <>
+        <button onClick={this.handleClick}>
+          {isVisible ? 'Unmount' : 'Mount'}
+        </button>
+        {isVisible && <StopWatch />}
+      </>
+    );
+  }
 }
 
 export default App;
