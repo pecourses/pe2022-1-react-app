c0de741bd25d9e7911929c881ee84a6aafa868c8
Viktoriia <pecourses>
2022-12-25 18:01:16 +0200

    Add UsersList with selection

--- src/components/UsersList/index.jsx
+++ src/components/UsersList/index.jsx
@@ -3,30 +3,52 @@ import { Component } from 'react';
 // Списки і ключі
 // 1 Реакт вміє рендерити масиви
 // 2 Кожен елемент, що повторюється, повинен мати проп key
+//   (унікальний і однозначний для кожного елементу масиву, найчастіше id)
+
+const usersData = [
+  { id: 1, firstName: 'Test', lastName: 'Testovich' },
+  { id: 2, firstName: 'John', lastName: 'Dou' },
+  { id: 3, firstName: 'Jane', lastName: 'Dou' },
+];
+
 class UsersList extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      users: [
-        { id: 1, firstName: 'Test', lastName: 'Testovich' },
-        { id: 2, firstName: 'John', lastName: 'Dou' },
-        { id: 3, firstName: 'Jane', lastName: 'Dou' },
-      ],
+      users: usersData.map(u => ({ ...u, isSelected: false })),
     };
   }
-  render() {
-    // const usersElements = this.state.users.map((u, i) => <li key={i}>{u}</li>);
+
+  selectUser = id => {
     const { users } = this.state;
+    const foundIndex = users.findIndex(u => u.id === id);
+
+    const newUsers = [...users];
+    newUsers[foundIndex] = {
+      ...newUsers[foundIndex],
+      isSelected: !newUsers[foundIndex].isSelected,
+    };
+
+    this.setState({ users: newUsers });
+  };
+
+  mapUser = (u, i) => {
+    const inlineStyle = {
+      backgroundColor: u.isSelected ? 'yellow' : 'transparent',
+    };
+
     return (
-      <ul>
-        {users.map((u, i) => (
-          <li key={u.id}>
-            {u.firstName} {u.lastName}
-          </li>
-        ))}
-      </ul>
+      <li key={u.id} style={inlineStyle} onClick={() => this.selectUser(u.id)}>
+        {u.firstName} {u.lastName}
+      </li>
     );
+  };
+
+  render() {
+    const { users } = this.state;
+
+    return <ul>{users.map(this.mapUser)}</ul>;
   }
 }
 
