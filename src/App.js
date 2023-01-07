92d9c8dc9ba3100f9a93bd01bb6881206996b51c
Viktoriia <pecourses>
2023-01-07 17:36:16 +0200

    Add login form

--- src/App.js
+++ src/App.js
@@ -1,37 +1,7 @@
-import { Component } from 'react';
-import { FaChromecast } from 'react-icons/fa';
-import ThemeButton from './components/ThemeButton';
-import UsersList from './components/UsersList';
+import LoginForm from './components/LoginForm';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isLight: true,
-    };
-  }
-
-  setTheme = () => {
-    const { isLight } = this.state;
-    this.setState({ isLight: !isLight });
-  };
-
-  render() {
-    const { isLight } = this.state;
-    return (
-      <>
-        <FaChromecast />
-        <ThemeButton isLight={isLight} changeTheme={this.setTheme} />
-        <UsersList isLight={isLight} />
-      </>
-    );
-  }
+function App() {
+  return <LoginForm />;
 }
-
-// App;
-//   ThemeButton; напис
-//   UsersList;
-//     UsersListItem; підсвітка
 
 export default App;
---
+++ src/components/LoginForm/index.jsx
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import styles from './LoginForm.module.css';
+// state -> input
+// state <- input
+//        ^
+//     onChange
+
+const INITIAL_VALUES = { email: '', password: '' };
+
+const LOGIN_FORM_REX_EXP = {
+  email: /^.+@.+$/,
+  password: /^(?=.*[A-Z].*)(?=.*[a-z].*)(?=.*\d.*)(?=.*[!@#$%^&*.].*).{8,20}$/,
+};
+
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: INITIAL_VALUES.email,
+      isEmailValid: false,
+      password: INITIAL_VALUES.password,
+      isPasswordValid: false,
+    };
+  }
+
+  handleEmailChange = ({ target: { value } }) => {
+    this.setState({
+      email: value,
+      isEmailValid: LOGIN_FORM_REX_EXP.email.test(value),
+    });
+  };
+
+  handlePasswordChange = ({ target: { value } }) => {
+    this.setState({
+      password: value,
+      isPasswordValid: LOGIN_FORM_REX_EXP.password.test(value),
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    // send request
+    this.setState(INITIAL_VALUES);
+  };
+
+  render() {
+    const { email, password, isEmailValid, isPasswordValid } = this.state;
+
+    const emailClassName = classNames(styles.input, {
+      [styles.inputValid]: isEmailValid,
+      [styles.inputInvalid]: !isEmailValid,
+    });
+
+    const passwordClassName = classNames(styles.input, {
+      [styles.inputValid]: isPasswordValid,
+      [styles.inputInvalid]: !isPasswordValid,
+    });
+
+    return (
+      <div className={styles.formContainer}>
+        <h1 className={styles.formHeader}>LoginForm</h1>
+        <form className={styles.loginForm} onSubmit={this.handleSubmit}>
+          <label className={styles.label}>
+            <span className={styles.inputName}>Email</span>
+            <input
+              className={emailClassName}
+              type="email"
+              name="email"
+              placeholder="your@mail"
+              value={email}
+              onChange={this.handleEmailChange}
+              autoFocus
+            />
+          </label>
+          <label className={styles.label}>
+            <span className={styles.inputName}>Password</span>
+            <input
+              className={passwordClassName}
+              type="password"
+              name="password"
+              value={password}
+              onChange={this.handlePasswordChange}
+            />
+          </label>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
