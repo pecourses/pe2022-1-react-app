f834774e8e5cb5c856452301df014cf5b6c8239e
Viktoriia <pecourses>
2023-01-08 17:36:47 +0200

    Add NamedList

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import AnimalSlider from './components/AnimalSlider';
+const NamedList = ({ name, children }) => {
+  return (
+    <>
+      <h3>{name}</h3>
+      <ul>{children}</ul>
+    </>
+  );
+};
 
+// реалізувати NamedList: іменований список із заголовком з name
+// та вмістом списку з контенту NamedList
+
+// Odd numbers: - name
+// 1            - контент
+// 3            - контент
+// 5            - контент
+
+// Проп children дозволяє ділитися версткою і стилями
 function App() {
-  return <AnimalSlider />;
+  return (
+    <NamedList name="Odd number">
+      <li>1</li>
+      <li>3</li>
+      <li>5</li>
+    </NamedList>
+  );
 }
 
 export default App;
