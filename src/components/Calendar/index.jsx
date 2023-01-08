55e5803c2449ddc8c8dfb37141b03e5d75b6debf
Viktoriia <pecourses>
2023-01-08 19:53:09 +0200

    Add Calendar basic functions

---
+++ src/components/Calendar/index.jsx
@@ -0,0 +1,71 @@
+import { Component } from 'react';
+import {
+  getDate,
+  getDay,
+  startOfMonth,
+  startOfWeek,
+  endOfMonth,
+  getWeek,
+  getYear,
+  addDays,
+  parse,
+  isEqual,
+  isSameMonth,
+} from 'date-fns';
+
+const WEEK_DAYS = ['SANDAY', 'M...', 'T', 'W', 'T', 'F', 'S'];
+
+class Calendar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentDate: new Date(),
+      date: new Date(),
+    };
+  }
+
+  render() {
+    const { date, currentDate } = this.state;
+
+    // поточне число
+    const currentDay = getDate(currentDate);
+    // день тижня
+    const currentWeekday = WEEK_DAYS[getDay(currentDate)];
+
+    // номер початкового тижня місяця (як тиждень першого дня місяці)
+    const startWeek = getWeek(startOfMonth(date));
+    // номер кінцевого тижня місяця (як тиждень останнього дня місяці)
+    const endWeek = getWeek(endOfMonth(date));
+
+    const monthDays = [];
+
+    for (let i = startWeek; i <= endWeek; i++) {
+      const weekDays = [];
+
+      // отримати дату першого дня тижня по номеру тижня
+      let startWeekDay = startOfWeek(
+        parse(`${i}`, 'w', new Date(getYear(date), 0, 1))
+      );
+
+      // отримати всі 7 дат тижня по першому дню тижня
+      for (let j = 0; j < 7; j++) {
+        weekDays.push(getDate(addDays(startWeekDay, j)));
+      }
+      monthDays.push(weekDays);
+    }
+
+    // перевірка, чи співпадають 2 дати (щоб підсвітити поточну дату в календарі)
+    const isDateEqual1 = isEqual(currentDate, new Date(2022, 0, 7));
+    const isDateEqual2 = isEqual(currentDate, currentDate);
+
+    // перевірка, чи відноситься дата до виводимого справа місяця
+    // (щоб відображати дати поточного місяця і не відображати або притіняти дати попереднього/наступного місяців)
+    const isTheSameMonth1 = isSameMonth(date, new Date(2021, 0, 7));
+    const isTheSameMonth2 = isSameMonth(date, new Date(2023, 0, 7));
+
+    return <div>Calendar</div>;
+  }
+}
+
+export default Calendar;
