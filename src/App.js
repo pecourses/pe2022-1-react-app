a95808d7bb1743c073d2b14e8e575d06c45cfca7
Viktoriia <pecourses>
2023-01-08 20:12:47 +0200

    Add FlexContainer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import ImageWrapper from './components/ImageWrapper';
+import FlexContainer from './components/FlexContainer';
+import ImageWrapper from './components/ImageWrapper/index';
 
-// Проп children дозволяє ділитися версткою і стилями
 function App() {
   return (
-    <ImageWrapper
-      // width="400px"
-      // height="300px"
-      title="image"
-      tabIndex={1}
-      className="container">
-      <img
-        src="https://images.pexels.com/photos/11046405/pexels-photo-11046405.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        alt="home"
-      />
-    </ImageWrapper>
+    <FlexContainer>
+      <ImageWrapper width="400px" height="300px">
+        <img
+          src="https://images.pexels.com/photos/11046405/pexels-photo-11046405.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="home"
+        />
+      </ImageWrapper>
+    </FlexContainer>
   );
 }
 
---
+++ src/components/FlexContainer/index.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+
+function FlexContainer(props) {
+  const { children, jc, ai, fd } = props;
+
+  const flexContainerStyles = {
+    display: 'flex',
+    justifyContent: jc,
+    alignItems: ai,
+    flexDirection: fd,
+  };
+
+  return <div style={flexContainerStyles}>{children}</div>;
+}
+
+FlexContainer.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.element).isRequired,
+  jc: PropTypes.oneOf([
+    'flex-start',
+    'flex-end',
+    'center',
+    'space-between',
+    'space-around',
+    'space-evenly',
+  ]),
+  ai: PropTypes.oneOf(['start', 'end', 'center', 'baseline', 'stretch']),
+  fd: PropTypes.oneOf(['row', 'row-reverse', 'column', 'column-reverse']),
+};
+
+FlexContainer.defaultProps = {
+  jc: 'center',
+  ai: 'center',
+  fd: 'column',
+};
+
+export default FlexContainer;
