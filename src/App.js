9d1807ca3c0255ff225abe86a6bfe4068a3a0c60
Viktoriia <pecourses>
2023-01-21 16:36:29 +0200

    Add dataLoadingHOC

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
-import React, { useState } from 'react';
+import PhonesLoadedList from './components/PhonesLoadedList';
 
 // HOC - компонент вищого порядку (функція, яка приймає і повертає компонент)
 function App() {
-  const InnerComponentWithNewProp = withNewProp(InnerComponent);
-
-  return <InnerComponentWithNewProp ownProp="ownProp" />;
+  return <PhonesLoadedList url="/data/phones.json" />;
 }
 
 export default App;
-
-function withNewProp(WrappedComponent) {
-  function NewComponent(props) {
-    const [newProp, setNewProp] = useState(10);
-
-    return <WrappedComponent newProp={newProp} {...props} />;
-  }
-
-  return NewComponent;
-}
-
-function InnerComponent(props) {
-  return (
-    <div>
-      {props.ownProp ?? 'Prop is missing'} {props.newProp}
-    </div>
-  );
-}
