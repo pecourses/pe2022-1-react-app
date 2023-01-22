8f0fe77698fc86051e720adaf2576d805bef63a0
Viktoriia <pecourses>
2023-01-22 16:33:55 +0200

    Add styled form

--- src/components/forms/ContactForm/index.jsx
+++ src/components/forms/ContactForm/index.jsx
@@ -1,7 +1,8 @@
+import classNames from 'classnames';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import React from 'react';
 import { CONTACT_VALIDATION_SCHEMA } from '../../../utils/validate/validationSchemas';
-
+import styles from './ContactForm.module.sass';
 // Name, Tel, Email, Birthday
 
 function ContactForm() {
@@ -19,27 +20,68 @@ function ContactForm() {
       onSubmit={handleSubmit}
       validationSchema={CONTACT_VALIDATION_SCHEMA}>
       {formikProps => {
+        const nameClassNames = classNames(styles.input, {
+          [styles.valid]: !formikProps.errors.name && formikProps.touched.name,
+          [styles.invalid]: formikProps.errors.name && formikProps.touched.name,
+        });
+
+        const phoneClassNames = classNames(styles.input, {
+          [styles.valid]:
+            !formikProps.errors.phone && formikProps.touched.phone,
+          [styles.invalid]:
+            formikProps.errors.phone && formikProps.touched.phone,
+        });
+
+        // other Fiels classes
+
         return (
-          <Form>
+          <Form className={styles.form}>
             <label>
               <span>Name: </span>
-              <Field type="text" name="name" placeholder="Yourname" autoFocus />
-              <ErrorMessage name="name" component="span" />
+              <Field
+                className={nameClassNames}
+                type="text"
+                name="name"
+                placeholder="Yourname"
+                autoFocus
+              />
+              <ErrorMessage
+                className={styles.error}
+                name="name"
+                component="span"
+              />
             </label>
             <label>
               <span>Phone Number:</span>
-              <Field type="tel" name="phone" placeholder="+380111111111" />
-              <ErrorMessage name="phone" component="span" />
+              <Field
+                className={phoneClassNames}
+                type="tel"
+                name="phone"
+                placeholder="+380111111111"
+              />
+              <ErrorMessage
+                className={styles.error}
+                name="phone"
+                component="span"
+              />
             </label>
             <label>
               <span>Email:</span>
               <Field type="email" name="email" placeholder="your@mail.com" />
-              <ErrorMessage name="email" component="span" />
+              <ErrorMessage
+                className={styles.error}
+                name="email"
+                component="span"
+              />
             </label>
             <label>
               <span>Birthday</span>
               <Field type="date" name="birthday" />
-              <ErrorMessage name="birthday" component="span" />
+              <ErrorMessage
+                className={styles.error}
+                name="birthday"
+                component="span"
+              />
             </label>
             <button type="submit">Save</button>
           </Form>
