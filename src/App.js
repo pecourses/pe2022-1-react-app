d7e06139bc25bbb16f6801d4ee92fc986add0709
Viktoriia <pecourses>
2023-01-22 15:26:30 +0200

    Add Contact Form structure

--- src/App.js
+++ src/App.js
@@ -1,46 +1,8 @@
-import React, { Component } from 'react';
-import CONSTANTS from './constants';
-import { ThemeContext } from './contexts';
-import styles from './App.module.sass';
-import classNames from 'classnames';
-import Footer from './components/FooterForHOC';
-import ThemeSwitcher from './components/ThemeSwitcherForHOC';
+import React from 'react';
+import ContactPage from './pages/ContactPage';
 
-const { LIGHT, DARK, PINK } = CONSTANTS.THEME;
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      theme: LIGHT,
-    };
-  }
-
-  setTheme = newTheme => {
-    this.setState({ theme: newTheme });
-  };
-
-  render() {
-    const { theme } = this.state;
-    const pageClassName = classNames({
-      [styles.lightTheme]: theme === LIGHT,
-      [styles.darkTheme]: theme === DARK,
-      [styles.pinkTheme]: theme === PINK,
-    });
-
-    return (
-      <ThemeContext.Provider value={{ theme, setTheme: this.setTheme }}>
-        <div className={pageClassName}>
-          <header>
-            <span>Header</span> <ThemeSwitcher />
-          </header>
-          <main>Main Content</main>
-          <Footer />
-        </div>
-      </ThemeContext.Provider>
-    );
-  }
+function App() {
+  return <ContactPage />;
 }
 
 export default App;
---
+++ src/components/forms/ContactForm/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// Name, Tel, Email, Birthday
+
+function ContactForm() {
+  return <div>ContactForm</div>;
+}
+
+export default ContactForm;
